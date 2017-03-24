91b3c24c8bab76c6c8f4f3149eb367f225436a1e
Tigran Tumasov <shugar348@gmail.com>
2017-03-24 23:22:53 +0300

    Responsive: learning section & fixes

--- app/initialize.js
+++ app/initialize.js
@@ -1,13 +1,18 @@
 import $     from "jquery";
 
 $(document).ready(() => {
-  $('.school-info__text span').on('click', function() {
+  $('.school-info__text span').on('click', function () {
     $(this).remove()
     $('.school-info__hidden').toggleClass('school-info__hidden--active')
   })
 
-  $('a[href^="#"]').on('click', function() {
-    let el = $(this).attr('href');
-    $('body').animate({scrollTop: $(el).offset().top}, 500);
-  });
+  $('a[href^="#"]').on('click', function () {
+    let el = $(this).attr('href')
+    $('body').animate({scrollTop: $(el).offset().top}, 500)
+  })
+
+  $('.learning-item').on('click', function () {
+    $(this).toggleClass('learning-item--active')
+    $(this).find('.learning-item__text').slideToggle()
+  })
 });
