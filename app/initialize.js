78b4fed00dd269da9a07817cdb4088860cf2c525
Tigran Tumasov <shugar348@gmail.com>
2017-03-24 21:00:31 +0300

    Desktop: 'up' button in footer added

--- app/initialize.js
+++ app/initialize.js
@@ -5,4 +5,9 @@ $(document).ready(() => {
     $(this).remove()
     $('.school-info__hidden').toggleClass('school-info__hidden--active')
   })
+
+  $('a[href^="#"]').on('click', function() {
+    let el = $(this).attr('href');
+    $('body').animate({scrollTop: $(el).offset().top}, 500);
+  });
 });
