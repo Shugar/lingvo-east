d571d13ea8149e47084b40b43bca206bd3338455
Tigran Tumasov <shugar348@gmail.com>
2017-03-25 19:27:00 +0300

    Responsive: mobile layout done

--- app/initialize.js
+++ app/initialize.js
@@ -1,5 +1,18 @@
 import $     from "jquery";
 
+function onMenuClick (item) {
+  $(item).on('click', function (e) {
+    e.stopPropagation()
+
+    $('.header-menu--mobile')
+      .css('display', 'none')
+      .show()
+      .removeClass('header-menu--active')
+      .delay(300)
+      .fadeOut(0)
+  })
+}
+
 $(document).ready(() => {
   $('.school-info__text span').on('click', function () {
     $(this).remove()
@@ -15,4 +28,15 @@ $(document).ready(() => {
     $(this).toggleClass('learning-item--active')
     $(this).find('.learning-item__text').slideToggle()
   })
+
+  $('.header-burger').on('click', function () {
+    $('.header-menu--mobile')
+      .css('display', 'flex')
+      .hide()
+      .fadeIn(0)
+      .addClass('header-menu--active')
+  })
+
+  onMenuClick('.header-menu__cross')
+  onMenuClick('.header-menu__item')
 });
